62e64438692467c399df546327c67de905dc8520
Tanvir Ramim <ramimhossan14@gmail.com>
2023-08-15 18:07:23 +0600

    finally done

--- withdraw.js
+++ withdraw.js
@@ -1,21 +1,33 @@
 document.getElementById('btn-withdraw').addEventListener('click',function(){
+    
     const withdrawField=document.getElementById('withdraw-field')
-    const withdrawAmountString=withdrawField.value ;
+    const withdrawAmountString=withdrawField.value ; 
     const withdrawAmount=parseFloat(withdrawAmountString)
+    withdrawField.value=""
+    if(isNaN(withdrawAmount)){
+        alert('Plase provide a valid Number')
+        return;
+    }
     const withdrawTotal =document.getElementById('withdraw-total')
     const withdrawTotalString=withdrawTotal.innerText
     const withdrawTotalNum=parseFloat(withdrawTotalString)
-    const newWithdrawTotal=withdrawAmount+withdrawTotalNum
-    withdrawTotal.innerText=newWithdrawTotal
+   
     
     const balanceTotalEle=document.getElementById('balance-total')
     const previousBalanceTotalString=balanceTotalEle.innerText;
     const previousBalanceTotal=parseFloat(previousBalanceTotalString)
-    
 
+    
+    if(withdrawAmount>previousBalanceTotal){
+        alert("Bap ar bank a taka nai")
+        return ;
+    }
+    const newWithdrawTotal=withdrawAmount+withdrawTotalNum
+    withdrawTotal.innerText=newWithdrawTotal
+         
     const currentBalanceTotal=previousBalanceTotal-withdrawAmount
     balanceTotalEle.innerText=currentBalanceTotal
     
 
-    withdrawField.value=""
+   
 })
